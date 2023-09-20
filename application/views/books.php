961f5d11b5bd6aef23c807bbfcf50e5bf923dd19
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2023-09-20 14:03:51 +0700

    add

--- application/views/books.php
+++ application/views/books.php
@@ -120,28 +120,8 @@
             <div class="footer-title">
                 Copyright © Mobilekit 2020. All Rights Reserved.
             </div>
-            <div>Mobilekit is PWA ready Mobile UI Kit Template.</div>
-            Great way to start your mobile websites and pwa projects.
-
-            <div class="mt-2">
-                <a href="javascript:;" class="btn btn-icon btn-sm btn-facebook">
-                    <ion-icon name="logo-facebook" role="img" class="md hydrated" aria-label="logo facebook"></ion-icon>
-                </a>
-                <a href="javascript:;" class="btn btn-icon btn-sm btn-twitter">
-                    <ion-icon name="logo-twitter" role="img" class="md hydrated" aria-label="logo twitter"></ion-icon>
-                </a>
-                <a href="javascript:;" class="btn btn-icon btn-sm btn-linkedin">
-                    <ion-icon name="logo-linkedin" role="img" class="md hydrated" aria-label="logo linkedin"></ion-icon>
-                </a>
-                <a href="javascript:;" class="btn btn-icon btn-sm btn-instagram">
-                    <ion-icon name="logo-instagram" role="img" class="md hydrated" aria-label="logo instagram"></ion-icon>
-                </a>
-                <a href="javascript:;" class="btn btn-icon btn-sm btn-whatsapp">
-                    <ion-icon name="logo-whatsapp" role="img" class="md hydrated" aria-label="logo whatsapp"></ion-icon>
-                </a>
-                <a href="#" class="btn btn-icon btn-sm btn-secondary goTop">
-                    <ion-icon name="arrow-up-outline" role="img" class="md hydrated" aria-label="arrow up outline"></ion-icon>
-                </a>
+            <div></div>
+            <div class="mt-2"> 
             </div>
 
         </div>
