e11167ff9fcaba6deabffcbd0fdfbbe813be8321
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2023-09-21 19:19:40 +0700

    add

--- application/models/Goodsitem.php
+++ application/models/Goodsitem.php
@@ -22,7 +22,7 @@ class Goodsitem extends CI_Model
 	public function GetDataAvailableTable()
 	{
 		$QueryString = " 
-        SELECT TableID,TableName,Description FROM tbl_Table
+        SELECT TableID,TableName,Description FROM tbl_Table where TableID not in (SELECT TableID FROM tbl_BookingSession where IsCheckOut = 0)
         ";
 		$query = $this->mssql->query($QueryString);
 		$Data = $query->result_array();
