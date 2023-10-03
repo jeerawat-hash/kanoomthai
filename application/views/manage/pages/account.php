e03f00c8cf341a45a25ea1817955aab0cdeb3f49
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2023-10-03 20:49:05 +0700

    addd

--- application/views/manage/pages/account.php
+++ application/views/manage/pages/account.php
@@ -76,9 +76,9 @@
                 "responsive": true,
                 "lengthChange": true,
                 "autoWidth": false,
-                // "pageLength": 10,
+                "pageLength": -1,
                 // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
-                "buttons": ["copy", "csv", "excel", "pdf", "print"],
+                "buttons": ["copy", "excel"],
                 dom: "<'row'<'col-sm-6'B><'col-sm-6'f>>" +
                     "<'row'<'col-sm-12'tr>>" +
                     "<'row'<'col-sm-5'i><'col-sm-7'p>>",
--- application/views/manage/pages/manage.php
+++ application/views/manage/pages/manage.php
@@ -77,10 +77,10 @@
                 "responsive": true,
                 "lengthChange": true,
                 "autoWidth": false,
-                // "pageLength": 10,
+                "pageLength": -1,
                 // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
-                "buttons": ["copy", "csv", "excel", "pdf", "print"],
-                dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
+                "buttons": ["copy", "excel"],
+                dom: "<'row'<'col-sm-6'B><'col-sm-6'f>>" +
                     "<'row'<'col-sm-12'tr>>" +
                     "<'row'<'col-sm-5'i><'col-sm-7'p>>",
                 columns: [{
