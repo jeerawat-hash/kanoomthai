ffe823ef3a408a63af37b6b9cfd684b518454f28
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2023-10-03 20:45:47 +0700

    add

--- application/views/manage/pages/account.php
+++ application/views/manage/pages/account.php
@@ -79,7 +79,7 @@
                 // "pageLength": 10,
                 // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
                 "buttons": ["copy", "csv", "excel", "pdf", "print"],
-                dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
+                dom: "<'row'<'col-sm-6'B><'col-sm-6'f>>" +
                     "<'row'<'col-sm-12'tr>>" +
                     "<'row'<'col-sm-5'i><'col-sm-7'p>>",
                 columns: [{
