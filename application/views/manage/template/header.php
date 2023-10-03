657ad02f570b7d7a0f413c6becb5496e95f9fe74
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2023-10-03 20:47:50 +0700

    add

--- application/views/manage/template/header.php
+++ application/views/manage/template/header.php
@@ -51,7 +51,7 @@ switch ($page_focus) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SakornCable Management | Dashboard</title>
+  <title>ร้านเท้งขนมไทยของแทร่</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
